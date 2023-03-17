5582a2fd00ef7245f5ff36319baa437e292a29ad
vaibhavxom <59342045+vaibhavxom@users.noreply.github.com>
2023-03-17 06:09:57 -0700

    Add files via upload

---
+++ StudentGradeCalculator/src/com/mile1/exception/NoNameException.java
@@ -0,0 +1,15 @@
+package com.mile1.exception;
+
+public class NoNameException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String toString() {
+		
+		return "name is null";
+	}
+
+}
---
+++ StudentGradeCalculator/src/com/mile1/exception/NullStudentObjectException.java
@@ -0,0 +1,14 @@
+package com.mile1.exception;
+
+public class NullStudentObjectException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String toString() {
+		
+		return"object is null";
+	}
+}
