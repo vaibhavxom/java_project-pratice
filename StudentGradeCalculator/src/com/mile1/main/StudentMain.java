6c875c9ef0e223c3cb61c96f90778dc01a881c65
vaibhavxom <59342045+vaibhavxom@users.noreply.github.com>
2023-03-20 10:35:48 +0530

    Add files via upload

--- StudentGradeCalculator/src/com/mile1/main/StudentMain.java
+++ StudentGradeCalculator/src/com/mile1/main/StudentMain.java
@@ -8,53 +8,57 @@ import com.mile1.service.StudentReport;
 import com.mile1.service.StudentService;
 
 public class StudentMain {
-	
 	static Student Data[]= new Student[4];
-	StudentMain()
+	public StudentMain()
 	{
 		
 	}
-	StudentMain(String s,int n,String g){
-		for(int i =0;i<Data.length;i++) 
+	
+	public StudentMain(Student data[]) {
+		
+		StudentMain.Data = data;
+		
+		for(int i=0;i<data.length;i++)
 		{
-			Data[i]=new Student();
+			data[i]=new Student();
 		}
 		
-		Data [0] = new Student("sekar",new int[]{70,75,95},null);
-		Data [1] = new Student(null,new int[]{70,75,95},"g");
-		Data [2] = null;
-		Data [3] = new Student("manoj",null,null);
+		data[0] = new Student("sekar", new int[]{85,50,95},"f");
+		data[1] = new Student(null,new int[]{85,50,95}, null);
+		data[2] = null;
+		data[3] = new Student("manoj", null, null);
+		
+		
 	}
-	public static void main(String[] args) {
 	
+	public static void main(String []args) {
 		StudentReport sr = new StudentReport();
-		StudentService ss =new StudentService();
-		System.out.println("grade Calculation");
+		StudentService ss = new StudentService();
+		
+		System.out.println("Grade Claculation");
 		
 		String x = null;
 		
-		for(int i=0 ; i <Data.length;i++)
-			{
-				try {
-					x = sr.validate(Data[i]);
-				}
-				catch (NoNameException ex) {
-					x = "NullNameException ";
-				}
-				catch (NullMarksArrayException ex) {
-					x = "NullMarksArrayException ";
-				}
-				catch (NullStudentObjectException ex) {
-					x = "NullStudentObjectException ";
-				}
-				
-				System.out.println("grade = " + x);
+		for(int i= 0;i < Data.length;i++)
+		{
+			try {
+				x = sr.validate(Data[i]);
+			} catch (NoNameException e) {
+				x = "no name exception occured";
+			} catch(NullMarksArrayException e) {
+				x = "null mark exception occured";
+			} catch(NullStudentObjectException e){
+				x = "null mark exception occured";
 			}
-		System.out.println("Number of object with the mark as null "+ ss.findNumberOfNullMarksArray(Data));
-		System.out.println("Number of object with the name as null "+ ss.findNumberOfNullName(Data));
-		System.out.println("Number of object with the object as null "+ ss.findNumberOfNullObjects(Data));
+			
+			System.out.println("grade"+ x);
+		}
 		
+		System.out.println("number of object with marks array as null = "+ ss.findNumberOfNullMarksArray(Data));
+		System.out.println("number of object with no name as null = "+ ss.findNumberOfNullName(Data));
+		System.out.println("number of object that are entirely  null = "+ ss.findNumberOfNullObjects(Data));
 		
 	}
+
 
 }
